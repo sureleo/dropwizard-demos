be183fd201a60b58842694c45b42e00adf33ef22
zyin-squareup <zyin@squareup.com>
2023-06-16 16:25:47 -0400

    Support Post and Get with different file sources

--- hello-world-example/src/test/com/gatling/loadtest/LoadTest.java
+++ hello-world-example/src/test/com/gatling/loadtest/LoadTest.java
@@ -17,32 +17,7 @@ import io.netty.handler.codec.http.HttpHeaderValues;
  * </ul>
  */
 public class LoadTest extends Simulation {
-    public ActionBuilder createHttpRequest (String method, String path, String body)
-    {
-        if ("POST".equalsIgnoreCase(method)) {
-            return http("Post")
-                .post("/"+path)
-                .header(HttpHeaderNames.CONTENT_TYPE, String.valueOf(HttpHeaderValues.APPLICATION_JSON))
-                .body(StringBody(body))
-                .check(
-                    status().is(201)
-                );
-        }
-        else if ("GET".equalsIgnoreCase(method)) {
-            return http("list employees")
-                .get("/"+path)
-                .check(status().is(200));
-        }
-        else {
-            return http("default list employees")
-                .get("/"+path)
-                .check(status().is(200));
-        }
-    }
 
-
-    FeederBuilder<String> feeder = ssv("com/gatling/resources/load-test.csv").random();
-    // todo generate these test classes based on the input format
     HttpProtocolBuilder httpProtocol =
         http.baseUrl("http://0.0.0.0:8080")
             .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
@@ -52,40 +27,44 @@ public class LoadTest extends Simulation {
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0"
             );
 
-    ChainBuilder edit =
+    FeederBuilder<String> post_feeder = ssv("com/gatling/resources/load-test-post.csv").random();
+
+    ChainBuilder post =
         tryMax(2).on(
-            feed(feeder)
+            feed(post_feeder)
             .exec(
-                createHttpRequest("#{METHOD}", "#{PATH}", "#{BODY}")
-                //http("Post")
-                //    .post("/employees")
-                //    .header(HttpHeaderNames.CONTENT_TYPE, String.valueOf(HttpHeaderValues.APPLICATION_JSON))
-                //    .body(StringBody("#{BODY}"))
-                //    .check(
-                //        status().is(201)
-                //    )
+                http("Post")
+                    .post("/#{PATH}")
+                    .header(HttpHeaderNames.CONTENT_TYPE, String.valueOf(HttpHeaderValues.APPLICATION_JSON))
+                    .body(StringBody("#{BODY}"))
+                    .check(
+                        status().is(201)
+                    )
             )
             .pause(1)
             .exitHereIfFailed()
         );
 
+    FeederBuilder<String> get_feeder = ssv("com/gatling/resources/load-test-get.csv").random();
+
     // repeat is a loop resolved at RUNTIME
     ChainBuilder browse =
         // Note how we force the counter name, so we can reuse it
-        repeat(4, "i").on(
-            exec(
+        repeat(2).on(
+            feed(get_feeder)
+            .exec(
                 http("list employees")
-                    .get("/employees")
+                    .get("/#{PATH}")
                     .check(status().is(200))
             ).pause(1)
         );
 
-    ScenarioBuilder users = scenario("Users").exec(edit);
+    ScenarioBuilder users = scenario("Users").exec(browse, post);
     //ScenarioBuilder admins = scenario("Admins").exec(search, browse, edit);
 
     {
         setUp(
-            users.injectOpen(rampUsers(2).during(30))
+            users.injectOpen(rampUsers(30).during(30))
             //admins.injectOpen(rampUsers(2).during(10))
         ).protocols(httpProtocol);
     }
